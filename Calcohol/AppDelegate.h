79d81d2d51ab71257e31fd3ff516a28cdde10f32
Peter Shultz <peterkshultz@verizon.net>
2014-11-03 01:12:34 -0500

    Initial Commit

---
+++ Calcohol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcohol
+//
+//  Created by Peter Shultz on 11/3/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcohol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcohol
+//
+//  Created by Peter Shultz on 11/3/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
